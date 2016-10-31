5a6bb7e7a916ceeaf66e7eb38d849b59a365f5c8
ntlk <hi@ntlk.net>
2016-10-31 16:46:54 +0000

    Set everything up

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
+const elasticsearch = require('elasticsearch');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+var connectionString = process.env.BONSAI_URL;
+
+var client = new elasticsearch.Client({
+    host: connectionString
+});
+
+app.set('port', port);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/:searchTerm', (req, res) => {
+  res.send('you are looking for ' + req.params.searchTerms);
+});
+
+app.listen(port, () => {
+  console.log('Example app listening on port ' + port);
 });
