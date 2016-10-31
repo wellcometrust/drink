54aafc06eeecce0e66e52be085f77820f6bde32a
ntlk <hi@ntlk.net>
2016-10-31 16:23:01 +0000

    Define a static directory

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-
 var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
