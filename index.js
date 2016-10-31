d6ac7c2a05fba4cdd5048b0c31e11f4c7e4fc875
ntlk <hi@ntlk.net>
2016-10-31 16:22:04 +0000

    Use the default port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-let express = require('express');
-let app = express();
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
